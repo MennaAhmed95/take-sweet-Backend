28db51690d2efece46e10a9eb6ceb36171b8788a
Hala Elsaka <halaelsaka2@gmail.com>
2020-06-11 00:03:29 +0200

    add middleware

---
+++ middleware/validation.js
@@ -0,0 +1,16 @@
+const {validationResult} = require('express-validator');
+require('express-async-errors');
+
+module.exports = (...validationChecks)=> async (req, re, next) => {
+    await Promise.all(
+        validationChecks.map(
+            check => check.run(req)
+        )
+    );
+    const {errors} = validationResult(req);
+    if (!errors.length) {
+        return next();
+    }
+    throw CustomError('ValidationError', 422, errors)
+
+}
