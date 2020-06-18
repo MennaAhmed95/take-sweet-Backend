d1874c79cc0e81426f6eff7588b569e9ef493c3b
Hala Elsaka <halaelsaka2@gmail.com>
2020-06-18 02:22:30 +0200

    update PaymentType model and route

--- routes/paymentType.js
+++ routes/paymentType.js
@@ -31,10 +31,41 @@ router.post('/addPaymentType', async (req, res, next) => {
     res.json(paymentType)
 
 })
+router.patch('/:id', async (req, res, next) => {
+    const id = req.params.id
+    const {
+        name,
+        src,
+        checked
+    } = req.body;
+    const paymentType = await PaymentType.findByIdAndUpdate(id, {
+        name,
+        src,
+        checked
+    }, {
+        new: true,
+        runValidators: true,
+        omitUndefined: true,
+    });
+    const paymentTypeList = await PaymentType.find();
+
+
+    res.status(200).json({
+        message: "payment Updated",
+        paymentType,paymentTypeList
+    })
+
+})
+
 
 //delete payment type by id
 router.delete('/:id', authenticationmiddleWare, async (req, res, next) => {
     const id = req.user.id;
     const paymentType = await PaymentType.findByIdAndDelete(id);
     res.status(200).json(paymentType)
+})
+router.get('/:id',  async (req, res, next) => {
+    const id = req.params.id;
+    const paymentType = await PaymentType.findById(id)
+    res.status(200).json(paymentType);
 })
